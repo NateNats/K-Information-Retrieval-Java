d686a3b3589ba4ce51e98f716102b854ee10cf14
NateNats <nicolaussagraha14@gmail.com>
2025-05-01 11:37:40 +0700

    add new function, showing terms with count of every term inside txt file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,12 @@
 package org.example;
 import java.io.IOException;
 import static org.example.ReadTokenize.readntokenize;
-
+import static org.example.ReadTokenize.readntokenizeV2;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            readntokenize();
+            readntokenizeV2();
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
--- src/main/java/org/example/ReadTokenize.java
+++ src/main/java/org/example/ReadTokenize.java
@@ -4,12 +4,11 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class ReadTokenize {
     static Map<String, ArrayList<String>> terms = new TreeMap<>();
+    static Map<String, NavigableMap<String, Integer>> termsV2 = new HashMap<>();
     static String willIgnore = "[.,!?:;'\"]";
 
     public static void readntokenize() throws IOException {
@@ -39,7 +38,6 @@ public class ReadTokenize {
                                 terms.get(token).add(file.getName().split("\\.")[0]);
                             }
 
-                            // terms.get(token).add(file.getName());
                         } else {
                             terms.put(token, new ArrayList<>());
                             terms.get(token).add(file.getName().split("\\.")[0]);
@@ -54,6 +52,56 @@ public class ReadTokenize {
 
         for (Map.Entry<String, ArrayList<String>> entry : terms.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+
+    public static void readntokenizeV2() throws IOException {
+
+        File folder = new File("src/main/resources");
+        File[] files = folder.listFiles();
+
+        assert files != null;
+        for (File file : files) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    String[] tokens = line.split(" ");
+                    for (String token : tokens) {
+                        if (token.contains(".") || token.contains(",") || token.contains("!") ||
+                                token.contains("?") || token.contains(":") || token.contains(";") ||
+                                token.contains("'") || token.contains("\"")) {
+                            token = token.replaceAll(willIgnore, "");
+                        }
+
+                        String filename = file.getName().split("\\.")[0];
+                        if (termsV2.containsKey(token)) {
+                            if (termsV2.get(token).containsKey(filename)) {
+                                termsV2.get(token).put(filename, termsV2.get(token).get(filename) + 1);
+                            } else {
+                                termsV2.get(token).put(filename, 1);
+                            }
+
+                        } else {
+                            termsV2.put(token, new TreeMap<>());
+                            termsV2.get(token).put(filename, 1);
+                        }
+
+                    }
+                }
+
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        for (Map.Entry<String, NavigableMap<String, Integer>> entry : termsV2.entrySet()) {
+            System.out.print(entry.getKey() + " --> ");
+
+            for (Map.Entry<String, Integer> docs : entry.getValue().entrySet()) {
+                System.out.print("(" + docs.getKey() + ": " + docs.getValue() + ") ");
+            }
+
+            System.out.println();
         }
     }
 
